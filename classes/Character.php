17b108edb64e0b6788ec87757c4239c068ce0b81
Renaud <renaud.hpavie@gmail.com>
2019-05-07 15:32:21 +0200

    Finition de la classe Character

--- classes/Character.php
+++ classes/Character.php
@@ -2,8 +2,27 @@
 
 class Character
 {
+  public $arms = 2;
   public $level;
   public $health;
   public $strength;
   public $maxSpeed;
+
+  public function __construct($a ,$b ,$c ,$d )
+  {
+    $this->level = $a;
+    $this->health = $b;
+    $this->strength = $c;
+    $this->maxSpeed = $d;
+  }
+
+  public function getHealth()
+  {
+    return $this->health;
+  }
+
+  public function __destruct()
+  {
+    echo ' BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM ';
+  }
 }
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 require_once './classes/Character.php';
+
+$debo = new Character(10,27,528,3);
+$vinzeckel = new Character(13,18,250,0.6);
+
+echo $debo->getHealth();
+
+$debo->health = 32;
+
+echo $debo->getHealth();
